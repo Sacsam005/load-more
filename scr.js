0bcfefc4d41fe153d9d6cba9ed4c4ea9a3e965ad
Sachin Samal <88958992+Sacsam005@users.noreply.github.com>
2022-01-14 23:16:46 -0600

    add

---
+++ scr.js
@@ -0,0 +1,17 @@
+let loadMoreBtn = document.querySelector('#load-more-btn');
+let currentItem = 3;
+
+loadMoreBtn.onclick = () => {
+    let boxes = [...document.querySelectorAll('.container .box-container')];
+
+    for (var i = currentItem; i < currentItem + 3; i++) {
+        boxes[i].style.display = "inline-block";
+    }
+
+    currentItem += 3;
+
+    if (currentItem >= boxes.length) {
+        loadMoreBtn.style.display = "none"
+    }
+
+}
